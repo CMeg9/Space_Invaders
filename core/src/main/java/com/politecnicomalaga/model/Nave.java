4247272ce96f9aa2388eb0be0ca83ea8c5eac6b3
IsraelMontiel777 <primerodesdoble.1@gmail.com>
2025-03-08 12:22:19 +0100

    Nave 1.0

--- core/src/main/java/com/politecnicomalaga/model/Nave.java
+++ core/src/main/java/com/politecnicomalaga/model/Nave.java
@@ -1,4 +1,29 @@
 package com.politecnicomalaga.model;
 
-public class Nave {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Nave extends ObjetoVolador {
+
+    public Nave(int iPosicionX, int iPosicionY, int iAlto, int iAncho, int iVelocidad, Texture tImg) {
+        super(iPosicionX, iPosicionY, iAlto, iAncho, iVelocidad, true, tImg);
+    }
+
+    public void disparar() {
+        // Crear un disparo en la posición actual de la nave
+        DisparoAmigo disparo = new DisparoAmigo(getiPosicionX() + getiAncho() / 2, getiPosicionY() + getiAlto(), new Texture("disparo.png"), 5);
+        // Aquí deberíamos añadir el disparo a una lista de disparos en la clase principal del juego
+    }
+
+    public boolean fallece() {
+        // Si la nave es golpeada, dejará de estar viva
+        setbVivo(false);
+        return true; // Indica que la nave ha sido destruida
+    }
+
+    @Override
+    public void draw(SpriteBatch spriteBatch) {
+        super.draw(spriteBatch);
+    }
 }
+
