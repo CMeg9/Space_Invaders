7c4def7cae8298347eb93f032718b9c6f22b5684
Saray <saraitareyess@gmail.com>
2025-03-10 19:02:00 +0100

    Actualización de la clase Disparo

--- core/src/main/java/com/politecnicomalaga/model/Disparo.java
+++ core/src/main/java/com/politecnicomalaga/model/Disparo.java
@@ -1,6 +1,14 @@
 package com.politecnicomalaga.model;
 
-public class Disparo {
+import com.badlogic.gdx.graphics.Texture;
+
+public class Disparo extends ObjetoVolador {
+
+    public Disparo(int iPosicionX, int iPosicionY, int iAlto, int iAncho, int iVelocidad, Texture tImg){
+
+        super(iPosicionX,iPosicionY,iAlto,iAncho,iVelocidad,true,tImg);
+
+    }
 
 
 
