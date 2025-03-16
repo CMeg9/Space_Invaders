8e0ef20fba746ab4e6722411023fb446d84cc426
Carlos Megias <carlosmegias200@gmail.com>
2025-03-17 00:42:51 +0100

    Inicio clase Enemigo

--- core/src/main/java/com/politecnicomalaga/model/Enemigo.java
+++ core/src/main/java/com/politecnicomalaga/model/Enemigo.java
@@ -1,4 +1,20 @@
 package com.politecnicomalaga.model;
 
-public class Enemigo {
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Enemigo extends Nave{
+
+    public Enemigo(int iPosicionX, int iPosicionY, int iAlto, int iAncho, int iVelocidad, Texture tImg) {
+        super(iPosicionX, iPosicionY, iAlto, iAncho, iVelocidad, tImg);
+    }
+
+
+    public void disparar() {
+        // Crear un disparo en la posición actual de la nave
+        DisparoEnemigo disparo = new DisparoEnemigo(getiPosicionX() + getiAncho() / 2, getiPosicionY() + getiAlto(), new Texture("disparo.png"), 5);
+        // Aquí deberíamos añadir el disparo a una lista de disparos en la clase principal del juego
+    }
+
+
 }
